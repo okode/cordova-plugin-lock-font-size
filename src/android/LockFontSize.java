37128fba3cc747c9565fd72b4e96a68c319dc129
Jorge Navarro <jnavarro@okode.com>
2020-05-05 14:02:18 +0200

    Finish implementation

--- src/android/LockFontSize.java
+++ src/android/LockFontSize.java
@@ -1,9 +1,13 @@
-package org.apache.cordova.smartlock;
+package org.apache.cordova.lockfontsize;
+
+import android.webkit.WebView;
 
 import java.util.Arrays;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 
 public class LockFontSize extends CordovaPlugin {
@@ -11,15 +15,22 @@ public class LockFontSize extends CordovaPlugin {
     private static final String[] actions = {"init"};
     private static final String TAG = "LockFontSize";
     private static final int NORMAL_TEXT_ZOOM = 100;
+    private WebView webView;
+
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+        this.webView = (WebView) webView;
+        super.initialize(cordova, webView);
+    }
 
     public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) {
         if (action.equals("init")) {
-            boolean isWebViewConfigAvailable = getBridge() != null && getBridge().getWebView() != null
-                && getBridge().getWebView().getSettings() != null;
-            if (isWebViewConfigAvailable) {
-                getBridge().getWebView().getSettings().setTextZoom(NORMAL_TEXT_ZOOM);
-            }
-            callbackContext.success();
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                public void run() {
+                    webView.getSettings().setTextZoom(NORMAL_TEXT_ZOOM);
+                    callbackContext.success();
+                }
+            });
         }
 
         return Arrays.asList(actions).contains(action);
